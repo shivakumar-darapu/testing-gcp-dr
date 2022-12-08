4d8906210932ee6d8c68969dd3cdcc617dc4a699
skdarapu <darapushivakumar@gmail.com>
2022-12-08 16:55:59 +0530

    Update VPC.tf

--- VPC.tf
+++ VPC.tf
@@ -32,7 +32,7 @@ resource "null_resource" "create-pri-to-sec-restore-plan" {
  provisioner "local-exec" {
 
     command = <<-EOF
-              gcloud beta container backup-restore backups list ${var.pri_to_sec_restore_plan_name} \
+              gcloud beta container backup-restore backups list \
               --project='grand-thought-365806' \
               --location='us-east1'
               --backup-plan='thd-us-west-to-us-east-backup-plan' \
